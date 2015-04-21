f6d6d9a254f31298a5aa8a365a148f347d08d23e
Vladimir <riuson@gmail.com>
2015-04-22 00:33:20 +0500

    Added category selector

--- components/PostEditor.php
+++ components/PostEditor.php
@@ -4,6 +4,7 @@ namespace Riuson\BlogFrontEnd\Components;
 use Cms\Classes\Page;
 use Cms\Classes\ComponentBase;
 use RainLab\Blog\Models\Post as PostModel;
+use RainLab\Blog\Models\Category as CategoryModel;
 use Riuson\BlogFrontEnd\Models\BlogPostUser as BlogPostUserModel;
 use Carbon\Carbon;
 
@@ -46,13 +47,24 @@ class PostEditor extends \RainLab\Blog\Components\Post
         $this->post_title = null;
         $this->post_excerpt = null;
         $this->post_content = null;
+        $this->selected_categories = null;
 
         if ($this->post != null) {
             $this->post_slug = $this->post->slug;
             $this->post_title = $this->post->title;
             $this->post_excerpt = $this->post->excerpt;
             $this->post_content = $this->post->content;
+
+            $categories = array();
+
+            foreach ($this->post->categories as $category) {
+                array_push($categories, $category->getKey());
+            }
+
+            $this->selected_categories = $categories;
         }
+
+        $this->categories = CategoryModel::orderBy('name', 'asc')->get();
     }
 
     public function onSubmit()
@@ -63,19 +75,20 @@ class PostEditor extends \RainLab\Blog\Components\Post
         $this->post_title = $title = post('input-title', '');
         $this->post_excerpt = $excerpt = post('input-excerpt', '');
         $this->post_content = $content = post('input-content', '');
+        $this->selected_categories = $categories = post('categories', '');
 
         if (empty($slug)) {
             $this->post_slug = $slug = \Str::slug($title);
         }
 
         if ($this->post == null) {
-            return $this->createNewPost($slug, $title, $excerpt, $content);
+            return $this->createNewPost($slug, $title, $excerpt, $content, $categories);
         } else {
-            return $this->updateExistingPost($slug, $title, $excerpt, $content);
+            return $this->updateExistingPost($slug, $title, $excerpt, $content, $categories);
         }
     }
 
-    private function createNewPost($slug, $title, $excerpt, $content)
+    private function createNewPost($slug, $title, $excerpt, $content, $categories)
     {
         if (! empty($slug)) {
             if ($this->isSlugUnique($slug)) {
@@ -94,6 +107,7 @@ class PostEditor extends \RainLab\Blog\Components\Post
                     $postuser->post_id = $post->getKey();
 
                     if ($postuser->save()) {
+                        $this->updateCategoriesForPost($post->getKey(), $categories);
                         $success = true;
                         $redirect = $this->property('redirectOnPost');
                         return \Redirect::to($redirect);
@@ -111,7 +125,7 @@ class PostEditor extends \RainLab\Blog\Components\Post
         }
     }
 
-    private function updateExistingPost($slug, $title, $excerpt, $content)
+    private function updateExistingPost($slug, $title, $excerpt, $content, $categories)
     {
         if (($this->post->slug != $slug) and (! $this->isSlugUnique($slug))) {
             $this->errorText = 'Slug not unique.';
@@ -124,6 +138,7 @@ class PostEditor extends \RainLab\Blog\Components\Post
         $this->post->content = $content;
 
         if ($this->post->save()) {
+            $this->updateCategoriesForPost($this->post->getKey(), $categories);
             $redirect = $this->property('redirectOnPost');
             return \Redirect::to($redirect);
         }
@@ -135,49 +150,32 @@ class PostEditor extends \RainLab\Blog\Components\Post
         return ($count == 0);
     }
 
-    public function onCheckTitle()
+    private static function updateCategoriesForPost($postID, $categoriesIDs)
     {
-        $title = post('input-title', '');
+        if ($postID != null) {
+            if (is_array($categoriesIDs) && count($categoriesIDs) > 0) {
+                // remove obsolete
+                \DB::table('rainlab_blog_posts_categories')->whereNotIn('category_id', $categoriesIDs)
+                    ->where('post_id', '=', $postID)
+                    ->delete();
 
-        $titleValid = false;
-        $slugValid = false;
-        $slug = '';
+                // add new
+                foreach ($categoriesIDs as $categoryID) {
+                    $exists = \DB::table('rainlab_blog_posts_categories')->where('post_id', '=', $postID)
+                        ->where('category_id', '=', $categoryID)
+                        ->first();
 
-        if (! empty($title)) {
-            $titleValid = true;
-            $slug = \Str::slug($title);
-
-            if (! empty($slug)) {
-                $unique = $this->isSlugUnique($slug);
-
-                if ($unique) {
-                    $slugValid = true;
+                    if ($exists == null) {
+                        \DB::insert('insert into rainlab_blog_posts_categories (post_id, category_id) values (?, ?);', [
+                            $postID,
+                            $categoryID
+                        ]);
+                    }
                 }
+            } else {
+                // remove all
+                \DB::table('rainlab_blog_posts_categories')->where('post_id', '=', $postID)->delete();
             }
         }
-
-        return [
-            'titleValid' => $titleValid,
-            'slug' => $slug,
-            'slugValid' => $slugValid
-        ];
-    }
-
-    public function onCheckSlug()
-    {
-        $slug = post('input-slug', '');
-        $slugValid = false;
-
-        if (! empty($slug)) {
-            $unique = $this->isSlugUnique($slug);
-
-            if ($unique) {
-                $slugValid = true;
-            }
-        }
-
-        return [
-            'slugValid' => $slugValid
-        ];
     }
 }
