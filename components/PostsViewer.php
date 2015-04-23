65f427c9aa0d2a1db426b692430fd23ebb37638f
Vladimir <riuson@gmail.com>
2015-04-24 01:21:06 +0500

    List blog posts what accessible to user's groups

--- components/PostsViewer.php
+++ components/PostsViewer.php
@@ -1,8 +1,8 @@
 <?php
-
 namespace Riuson\BlogFrontEnd\Components;
 
 use Cms\Classes\ComponentBase;
+use RainLab\Blog\Models\Post as PostModel;
 
 class PostsViewer extends \RainLab\Blog\Components\Posts
 {
@@ -40,5 +40,58 @@ class PostsViewer extends \RainLab\Blog\Components\Posts
                 }
             }
         }
+    }
+
+    protected function listPosts()
+    {
+        $categories = $this->category ? $this->category->id : null;
+        /*
+         * $posts = PostModel::with('categories.groups')->listFrontEnd([
+         * 'page' => $this->property('pageNumber'),
+         * 'sort' => $this->property('sortOrder'),
+         * 'perPage' => $this->property('postsPerPage'),
+         * 'categories' => $categories
+         * ]);
+         */
+
+        $userGroups = \Riuson\ACL\Classes\Acl::userGroups();
+
+        $blockedCategories = \DB::table('rainlab_blog_categories')->whereIn('rainlab_blog_categories.id', function ($query) {
+            $query->select('riuson_eveapiuser_blog_category_groups.category_id')
+                ->from('riuson_eveapiuser_blog_category_groups');
+        })
+            ->whereNotIn('rainlab_blog_categories.id', function ($query2) use($userGroups) {
+            $query2->select('riuson_eveapiuser_blog_category_groups.category_id')
+                ->from('riuson_eveapiuser_blog_category_groups')
+                ->leftJoin('riuson_acl_groups', 'riuson_acl_groups.id', '=', 'riuson_eveapiuser_blog_category_groups.group_id')
+                ->whereIn('riuson_acl_groups.name', $userGroups); // ['academy']
+        })
+            ->lists('rainlab_blog_categories.id');
+
+        /*
+         * List all accessible posts, eager load their categories
+         */
+        $posts = PostModel::with('categories')->whereIn('id', function ($query) use($blockedCategories) {
+            $query->select('post_id')
+                ->from('rainlab_blog_posts_categories')
+                ->whereNotIn('category_id', $blockedCategories);
+        })
+            ->listFrontEnd([
+            'page' => $this->property('pageNumber'),
+            'sort' => $this->property('sortOrder'),
+            'perPage' => $this->property('postsPerPage'),
+            'categories' => $categories
+        ]);
+
+        /*
+         * Add a "url" helper attribute for linking to each post and category
+         */
+        $posts->each(function ($post) {
+            $post->setUrl($this->postPage, $this->controller);
+            $post->categories->each(function ($category) {
+                $category->setUrl($this->categoryPage, $this->controller);
+            });
+        });
+        return $posts;
     }
 }
