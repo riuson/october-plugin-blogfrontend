d453f7e6ca13f2eae93c6fa3fe1d04140ff7dde0
Vladimir <riuson@gmail.com>
2015-04-30 22:37:38 +0500

    Added translation for plugin

---
+++ lang/en/lang.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+    'plugin' => [
+        'name' => 'Blog Front-End',
+        'description' => 'Front-End interface for RainLab.Blog.',
+    ],
+    'backend' => [
+        'field_groups_label' => 'Groups',
+        'field_groups_comment_above' => 'Specify which groups can view posts in this category.',
+        'field_groups_tab_name' => 'Groups',
+    ],
+];
---
+++ lang/ru/lang.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'plugin' => [
+        'name' => 'Blog Front-End',
+        'description' => 'Front-End надстройка к RainLab.Blog.'
+    ],
+    'backend' => [
+        'field_groups_label' => 'Группы',
+        'field_groups_comment_above' => 'Укажите группы, которым будет предоставлен доступ к сообщениям в этой категории.',
+        'field_groups_tab_name' => 'Группы',
+    ],
+];
+
